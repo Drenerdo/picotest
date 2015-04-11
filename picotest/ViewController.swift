5858a9518d4f51f5f9b1a7843cde000ebebe8736
Andre Smith <andre@suitefolio.com>
2015-04-11 13:46:25 -0400

    First commit to an awesome app

--- picotest/ViewController.swift
+++ picotest/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var durangotest: UIView!
+    
+    @IBAction func testdurango(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to durango", message: "Durango is listening", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
